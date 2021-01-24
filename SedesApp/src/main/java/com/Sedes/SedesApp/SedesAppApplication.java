a0dbb661bcff488e0337ffa7bab7bf844557665c
Bad-Grandpa <igor.mikolajek.255@gmail.com>
2021-01-24 21:22:27 +0100

    A SedesApp
    Description

---
+++ SedesApp/src/main/java/com/Sedes/SedesApp/SedesAppApplication.java
@@ -0,0 +1,13 @@
+package com.Sedes.SedesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SedesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SedesAppApplication.class, args);
+	}
+
+}
---
+++ SedesApp/src/test/java/com/Sedes/SedesApp/SedesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sedes.SedesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SedesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
